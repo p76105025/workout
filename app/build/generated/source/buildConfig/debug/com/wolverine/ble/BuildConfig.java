e62c909d9ef83df24e89fbd59720df3642ec8e35
p76105025 <p76105025@gs.ncku.edu.tw>
2021-11-21 20:40:53 +0800

    first push from lab

---
+++ app/build/generated/source/buildConfig/debug/com/wolverine/ble/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.wolverine.ble;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.wolverine.ble";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 43;
+  public static final String VERSION_NAME = "2.0";
+}
